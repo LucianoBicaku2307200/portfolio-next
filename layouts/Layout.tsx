bf02e58f1ea64aaa71d006af736f8f8a531e3a5f
LucianoBicaku2307200 <lucianobicaku23072000@gmail.com>
2023-02-03 13:41:14 +0100

    m

--- layouts/Layout.tsx
+++ layouts/Layout.tsx
@@ -4,8 +4,7 @@ import Footer from "../components/Footer";
 export default function Layout({ children }: any) {
   return (
     <div className="relative">
-      <Header /> 
-      
+      <Header />
       <main className="h-screen w-screen pb-[58px] pt-[58px]">
         {children}
       </main>
